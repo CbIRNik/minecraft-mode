089567df3c822ca7e13f41b96c5ceef643756df2
peoka <naaaaameeeeeee@gmail.com>
2025-12-24 00:44:59 +0300

    сделал чтобы пушка ставила столбик из 2х блоков камня, позже это будет портал

--- src/main/java/com/infdimmod/items/custom/PortalGun.java
+++ src/main/java/com/infdimmod/items/custom/PortalGun.java
@@ -1,10 +1,14 @@
 package com.infdimmod.items.custom;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
@@ -15,17 +19,42 @@ public class PortalGun extends Item {
         super(settings);
     }
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (world.isClient) {
-            return TypedActionResult.pass(user.getStackInHand(hand));
-        }
-        BlockPos frontOfPlayer = user.getBlockPos().offset(user.getHorizontalFacing(), 10);
-        LightningEntity lightningBolt = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
-        lightningBolt.setPosition(frontOfPlayer.toCenterPos());
-        world.spawnEntity(lightningBolt);
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos clickedPos = context.getBlockPos();
+        PlayerEntity player = context.getPlayer();
 
-        return TypedActionResult.success(user.getStackInHand(hand));
+
+
+
+
+        // Проверяем, что действие происходит на сервере
+        if (!world.isClient() && player != null) {
+            // Получаем позицию над кликнутым блоком
+            BlockPos posAbove = clickedPos.up();
+
+            // Проверяем, можно ли разместить блоки
+            if (canPlaceBlock(world, posAbove) && canPlaceBlock(world, posAbove.up())) {
+
+                // Ставим первый блок камня
+                world.setBlockState(posAbove, Blocks.STONE.getDefaultState());
+
+                // Ставим второй блок камня сверху
+                world.setBlockState(posAbove.up(), Blocks.STONE.getDefaultState());
+
+                return ActionResult.SUCCESS;
+            }
+        }
+
+        return ActionResult.PASS;
     }
 
-
+    private boolean canPlaceBlock(World world, BlockPos pos) {
+        // Проверяем, что блок можно заменить
+        BlockState state = world.getBlockState(pos);
+        return state.isAir() ||
+                state.isReplaceable() ||
+                !state.isFullCube(world, pos);
+    }
 }
+
