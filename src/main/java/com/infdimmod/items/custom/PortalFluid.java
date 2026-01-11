6b11654ce1c71edeb4fc7832ec4a73889c260c5b
peoka <naaaaameeeeeee@gmail.com>
2026-01-11 23:11:40 +0300

    67

--- src/main/java/com/infdimmod/items/custom/PortalFluid.java
+++ src/main/java/com/infdimmod/items/custom/PortalFluid.java
@@ -3,7 +3,6 @@ package com.infdimmod.items.custom;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registries;
 import net.minecraft.world.World;
 import net.minecraft.util.Hand;
